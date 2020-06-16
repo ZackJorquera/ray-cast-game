95458ff264e4cf41ee9ba0d4e1481b22350a8f71
ZackJorquera <jorquerazack@gmail.com>
2020-06-16 15:30:03 -0600

    made play space 10X10 and fixed bugs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 use std::time;
 use std::collections::HashMap;
-//#[macro_use]
 use glium::{glutin, Surface, Display, Program, Frame};
-use std::f32;
 
-static GAME_HEIGHT: usize = 8;
-static GAME_WIDTH: usize = 8;
-static GAME: [u8;64] = [
-    1,1,1,1,1,1,1,1,
-    1,0,1,0,0,0,0,0,
-    1,0,1,0,1,1,0,1,
-    1,0,0,0,1,0,0,1,
-    1,1,2,0,1,0,1,1,
-    1,0,0,0,1,0,0,1,
-    1,0,1,0,1,1,0,1,
-    1,1,1,1,1,1,1,1,
+static GAME_HEIGHT: usize = 10;
+static GAME_WIDTH: usize = 10;
+static GAME: [u8;100] = [
+    1,1,1,1,1,1,1,1,1,1,
+    1,0,1,0,0,0,0,1,0,1,
+    1,0,1,0,1,1,0,0,0,1,
+    1,0,0,0,1,0,0,1,0,1,
+    1,1,2,0,1,0,1,1,0,1,
+    1,0,0,0,1,0,0,1,0,1,
+    1,0,1,0,1,1,0,1,0,1,
+    1,1,0,1,1,1,0,1,0,1,
+    1,0,0,0,0,0,0,1,0,1,
+    1,1,1,1,1,1,1,1,1,1,
 ];
 
 static MOVE_SPEED: f32 = 1.0/3.0;
 static LOOK_SPEED: f32 = 2.0;
 
 static RAYS: usize = 60;
+static FOV: f32 = 1.6;
 
-static START_POS: PlayerPos = PlayerPos { position: [0.35, 0.2], dir: 1.0 };
+static START_POS: PlayerPos = PlayerPos { position: [0.35, 0.3], dir: 2.0 };
 
 #[derive(Copy, Clone)]
 struct Vertex 
@@ -41,9 +42,9 @@ struct PlayerPos
 fn draw_rect(top_left: Vertex, bottom_right: Vertex, color: (f32,f32,f32), target: &mut Frame, display: &Display, program: &Program)
 {
     // from two triangles
-    let vertex1 = top_left.clone();
+    let vertex1 = top_left;
     let vertex2 = Vertex { position: [ bottom_right.position[0],  top_left.position[1]] };
-    let vertex3 = bottom_right.clone();
+    let vertex3 = bottom_right;
     let vertex4 = Vertex { position: [ top_left.position[0], bottom_right.position[1]] };
     let shape1 = vec![vertex1, vertex2, vertex4];
     let shape2 = vec![vertex2, vertex3, vertex4];
@@ -76,23 +77,38 @@ fn draw_line(v1: Vertex, v2: Vertex, color: (f32,f32,f32), target: &mut Frame, d
 fn at_wall(pos: (f32, f32), horz: bool) -> u8
 {
     let pos2 = ((pos.0 + 1.0) * (GAME_WIDTH as f32) / 2.0, (pos.1 + 1.0) * (GAME_WIDTH as f32) / 2.0);
-    let (col, row) = (f32::floor(pos2.0) as usize, f32::floor(pos2.1) as usize);
+    let (mut col, mut row) = (f32::floor(pos2.0) as usize, f32::floor(pos2.1) as usize);
     if row as usize * GAME_WIDTH + col < GAME_WIDTH * GAME_HEIGHT
     {
+        let v1;
+        let v2;
         if horz
         {
-            let v1 = GAME[row as usize * GAME_WIDTH + col as usize];
-            let v2 = if row == 0 {v1} else { GAME[(row - 1) as usize * GAME_WIDTH + col as usize] };
-            
-            u8::max(v1,v2)
+            row = pos2.1.round() as usize;
+            v1 = GAME[row * GAME_WIDTH + col];
+            if row != 0
+            {
+                v2 = GAME[(row-1) * GAME_WIDTH + col];
+            }
+            else
+            {
+                v2 = v1;
+            }
         }
         else
         {
-            let v1 = GAME[row as usize * GAME_WIDTH + col as usize];
-            let v2 = if col == 0 {v1} else { GAME[row as usize * GAME_WIDTH + col - 1 as usize] };
-
-            u8::max(v1,v2)
+            col = pos2.0.round() as usize;
+            v1 = GAME[row * GAME_WIDTH + col];
+            if col != 0
+            {
+                v2 = GAME[row * GAME_WIDTH + (col - 1)];
+            }
+            else
+            {
+                v2 = v1;
+            }
         }
+        u8::max(v1,v2)
     }
     else
     {
@@ -136,12 +152,12 @@ fn calc_dist_to_wall(player_pos: &PlayerPos, angle: f32) -> (f32, bool, u8)
         ray_x = (ray_y - player_pos.position[1]) / f32::tan(angle) + player_pos.position[0];
         xoffset = yoffset / f32::tan(angle);
 
-        while ray_x >= -1.0 && ray_x <= 1.0 && ray_y >= -1.0 && ray_x <= 1.0 && f32::sin(angle) != 0.0
+        while ray_x >= -1.0 && ray_x <= 1.0 && ray_y >= -1.0 && ray_y <= 1.0 && f32::sin(angle) != 0.0
         {
             horz_wall = at_wall((ray_x, ray_y), true);
             if horz_wall > 0
             {
-                dist_to_horz = f32::sqrt(f32::powf(ray_y - player_pos.position[1], 2.0) + f32::powf(ray_x - player_pos.position[0], 2.0));
+                dist_to_horz = f32::sqrt((ray_y - player_pos.position[1]).powf(2.0) + (ray_x - player_pos.position[0]).powf(2.0));
                 break;
             }
             ray_y += yoffset;
@@ -169,12 +185,12 @@ fn calc_dist_to_wall(player_pos: &PlayerPos, angle: f32) -> (f32, bool, u8)
         ray_y = (ray_x - player_pos.position[0]) * f32::tan(angle) + player_pos.position[1];
         yoffset = xoffset * f32::tan(angle);
 
-        while ray_x >= -1.0 && ray_x <= 1.0 && ray_y >= -1.0 && ray_x <= 1.0 && f32::cos(angle) != 0.0
+        while ray_x >= -1.0 && ray_x <= 1.0 && ray_y >= -1.0 && ray_y <= 1.0 && f32::cos(angle) != 0.0
         {
             vert_wall = at_wall((ray_x, ray_y), false);
             if vert_wall > 0
             {
-                dist_to_vert = f32::sqrt(f32::powf(ray_y - player_pos.position[1], 2.0) + f32::powf(ray_x - player_pos.position[0], 2.0));
+                dist_to_vert = f32::sqrt((ray_y - player_pos.position[1]).powf(2.0) + (ray_x - player_pos.position[0]).powf(2.0));
                 break;
             }
             ray_y += yoffset;
@@ -182,6 +198,7 @@ fn calc_dist_to_wall(player_pos: &PlayerPos, angle: f32) -> (f32, bool, u8)
         }
     }
 
+    // pick shortest
     if dist_to_horz < dist_to_vert
     {
         (dist_to_horz, true, horz_wall)
@@ -201,18 +218,21 @@ fn ray_cast(player_pos: &PlayerPos, rays: usize, fov: f32) -> Vec<(usize, f32, f
         .collect()
 }
 
+// TODO: dont re draw calc/create the rects every time
 fn draw_3d_game(display: &Display, program: &Program, player_pos: &PlayerPos)
 {
     let mut target = display.draw();
     target.clear_color(0.0, 0.0, 1.0, 1.0);
+    draw_rect(Vertex{ position: [-1.0,1.0]}, Vertex{ position: [1.0,0.0]}, (0.5, 0.5, 0.5), &mut target, display, program);
 
     let rays = RAYS;
 
-    for (i, ray_ang, ray_dist, horz, wall) in ray_cast(player_pos, rays, 1.6)
+    for (i, ray_ang, ray_dist, horz, wall) in ray_cast(player_pos, rays, FOV)
     {
-        let dist = ray_dist*f32::cos(f32::abs(ray_ang - player_pos.dir));
+        if ray_dist > 100.0 || wall == 0 { continue; }
+        // I want to make the walls look more linear but I cant seem to figure out how.
+        let dist = ray_dist*f32::cos(f32::abs(ray_ang - player_pos.dir))/f32::cos(f32::abs(ray_ang - player_pos.dir)/10.0);
         let height = 0.2 / dist;
-        //println!("{}, {}", dist, height);
 
         let tl = Vertex { position: [(rays-i) as f32 * 2.0 / rays as f32 - 1.0, 0.0 + height] };
         let br = Vertex { position: [(rays-i-1) as f32 * 2.0 / rays as f32 - 1.0, 0.0 - height] };
@@ -222,10 +242,10 @@ fn draw_3d_game(display: &Display, program: &Program, player_pos: &PlayerPos)
             if horz { (0.0, 0.8, 0.0) } 
             else { (0.0, 1.0, 0.0) }
         } 
-        else 
-        { 
+        else
+        {
             if horz { (0.8, 0.0, 0.0) } 
-            else { (1.0, 0.0, 0.0) } 
+            else { (1.0, 0.0, 0.0) }
         };
 
         draw_rect(tl, br, color, &mut target, display, program);
@@ -268,17 +288,19 @@ fn draw_2d_game(display: &Display, program: &Program, player_pos: &PlayerPos)
 
     // draw player
     let player_size = 0.05;
-    let player_ver = Vertex {position: player_pos.position.clone() };
+    let player_ver = Vertex {position: player_pos.position };
     let player_tl = Vertex { position: [player_pos.position[0] - player_size/2.0, player_pos.position[1] - player_size/2.0] };
     let player_br = Vertex { position: [player_pos.position[0] + player_size/2.0, player_pos.position[1] + player_size/2.0] };
     let player_dir = Vertex { position: [player_pos.position[0] + 0.1*f32::cos(player_pos.dir), player_pos.position[1] + 0.1*f32::sin(player_pos.dir)] };
     draw_rect(player_tl, player_br, (0.1, 0.9, 0.1), &mut target, display, program);
-    draw_line(player_ver.clone(), player_dir, (1.0,1.0,0.0), &mut target, display, program);
+    draw_line(player_ver, player_dir, (1.0,1.0,0.0), &mut target, display, program);
 
     // draw rays
-    for (_, ray_ang, ray_dist, _, wall) in ray_cast(player_pos, RAYS, 1.5)
+    for (_, ray_ang, ray_dist, _, wall) in ray_cast(player_pos, RAYS, FOV)
     {
-        let color = if wall == 2 { (0.0, 1.0, 0.0) } else { (1.0, 0.0, 0.0) };
+        let color = if wall == 2 { (0.0, 1.0, 0.0) } 
+            else if wall == 1 { (1.0, 0.0, 0.0) } 
+            else { (0.0, 0.0, 0.0)};
         let ray_dir_ver = Vertex { position: [player_pos.position[0] + ray_dist*f32::cos(ray_ang), player_pos.position[1] + ray_dist*f32::sin(ray_ang)] };
         
         draw_line(player_ver, ray_dir_ver, color, &mut target, display, program);
@@ -328,10 +350,10 @@ fn handle_keys(keys: &HashMap<glutin::event::VirtualKeyCode,glutin::event::Virtu
 }
 
 fn main() {
-    let draw_3d = std::env::args().nth(1).unwrap_or(String::from("3d")).to_lowercase() != "2d";
+    let draw_3d = std::env::args().nth(1).unwrap_or_else(|| String::from("3d")).to_lowercase() != "2d";
 
     let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
+    let wb = glutin::window::WindowBuilder::new().with_title("Ray Trace Game");
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
@@ -378,22 +400,22 @@ fn main() {
                 },
                 _ => return,
             },
-            glutin::event::Event::DeviceEvent {event, ..} => match event
+            glutin::event::Event::DeviceEvent {event, ..} => 
             {
-                glutin::event::DeviceEvent::Key(key) => 
+                if let glutin::event::DeviceEvent::Key(key) = event
                 {
                     if let Some(letter) = key.virtual_keycode  
                     { 
                         if glutin::event::ElementState::Pressed == key.state
                         { 
-                            if !keys_down.contains_key(&letter)
-                            { let _ = keys_down.insert(letter, letter); }
+                            keys_down.entry(letter).or_insert(letter);
                         }
                         else
-                        { let _ = keys_down.remove(&letter); }
+                        {
+                            let _ = keys_down.remove(&letter);
+                        }
                     }
-                },
-                _ => ()
+                }
             },
             _ => (),
         }
